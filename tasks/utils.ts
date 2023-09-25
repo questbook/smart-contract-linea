74fdd7c286707f5aecd8c3005d191ca218c8c1b0
Hasan Khadra <59062695+hasankhadra@users.noreply.github.com>
2023-09-25 21:30:51 +0300

    add unit-test in github actions on main branch

--- tasks/utils.ts
+++ tasks/utils.ts
@@ -1,11 +1,11 @@
-import CONTRACTS_CONFIG from '../../resources/contracts/config.json'
+// import CONTRACTS_CONFIG from '../../resources/contracts/config.json'
 
-export function getContractAddress(networkName: string) {
-	for(const { chainName, address } of Object.values(CONTRACTS_CONFIG)) {
-		if(chainName === networkName) {
-			return address
-		}
-	}
+// export function getContractAddress(networkName: string) {
+// 	for(const { chainName, address } of Object.values(CONTRACTS_CONFIG)) {
+// 		if(chainName === networkName) {
+// 			return address
+// 		}
+// 	}
 
-	throw new Error(`No contract address found for network "${networkName}"`)
-}
+// 	throw new Error(`No contract address found for network "${networkName}"`)
+// }
